39287e34abb0ed43b7979fb914e9b41bf1eec2bf
shashank <shashanksinghnegi12@gmail.com>
2024-05-29 16:49:21 +0530

    added transaltion data for footer

--- coelian/src/app/_components/blogs/Blogs.jsx
+++ coelian/src/app/_components/blogs/Blogs.jsx
@@ -37,14 +37,14 @@ const Blogs = (props, ref) => {
   return (
     <div className="blogs  flex blogBackground justify-center" ref={blogsRef}>
       <section class="pt-20 lg:pt-[120px] pb-10 lg:pb-20">
-        <div className="container">
+        <div className="container grid grid-cols-2 gap-4">
           {blogs?.map((blog, index) => (
-            <div key={index}>
+            <div key={index} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               
-                <div>
+                <div className="p-5">
                   <img src={images[index]} alt={blog.title} />
-                  <h1>{blog.title}</h1>
-                  <p>{blog.body}</p>
+                  <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{blog.title}</h1>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{blog.body}</p>
                 </div>
              
             </div>
